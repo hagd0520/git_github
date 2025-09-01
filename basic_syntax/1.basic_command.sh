6761d2dfd27dfd81d2c01364f18a5c528b7c5412
hagd0520 <hagd0520@gmail.com>
2025-09-01 20:06:34 +0900

    new commit

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -43,3 +43,14 @@ git checkout 커밋Id
 
 # 다시 현재의 브랜치의 commit 되돌아올 때
 git checkout 현재브랜치명(main)
+
+# git작업 취소
+# commit 이후의 취소
+git reset head~1
+git reset head^
+
+# staging area로 까지만 취소
+git reset --soft head~1
+
+# push 이후의 취소 -> commit 메시지 작성 vi 화면으로 이동 -> :wq!
+git revert 기존의커밋ID
